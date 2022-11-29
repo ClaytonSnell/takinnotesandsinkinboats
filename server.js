887d3478ec6d628df92ecac0d0b8f25abe5977c3
claytontsnell@gmail.com <claytontsnell@gmial.com>
2022-11-29 09:47:53 -0500

    finished

--- server.js
+++ server.js
@@ -1,20 +1,66 @@
 const express = require('express');
 const path = require('path');
-const routes = require('./routes');
-
-const PORT = process.env.port || 3001;
+const fs = require('fs');
+const util = require('util');
+const notes = require('./db/db.json');
+const uuid = require('uuid');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-
-// Middleware for parsing JSON and urlencoded form data
+//Middleware for parsing the application
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(routes);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+//Setting up the API route/ GET method
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, './db/db.json'))
+}
+);
+
+app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+    newNote.id = uuid.v4()
+    console.log(newNote)
+    notes.push(newNote)
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes))
+    res.sendFile(path.join(__dirname, './db/db.json'))
+});
 
 
+// delete method
+app.delete("/api/notes/:id", (req, res) => {
+const deleteId = req.params.id;
+
+for(let i = 0; i < notes.length; i++) {
+    console.log(notes[i].id, deleteId)
+    if (notes[i].id === deleteId) {
+        notes.splice(i, 1);
+    
+    }
+}
+console.log(notes);
+
+fs.writeFileSync('./db/db.json', JSON.stringify(notes), error => {
+    if (error) throw error;
+   
+});
+res.json(notes);
+});
+
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
 
 
 app.listen(PORT, () =>
